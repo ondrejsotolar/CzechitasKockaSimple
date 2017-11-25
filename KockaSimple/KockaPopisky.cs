4d4f3b04fef8fad6dc9b62a82df4e87a45bc5491
ondrejsotolar <ondrej.sotolar@gmail.com>
2017-11-25 22:05:20 +0100

    Update labels

--- KockaSimple/KockaPopisky.cs
+++ KockaSimple/KockaPopisky.cs
@@ -3,19 +3,21 @@
     public class KockaPopisky
     {
         public const string PocetTydnuCS = "Počet týdnů";
-        private const string PocetTydnuEN = "Number of weeks";
+        public const string PocetTydnuEN = "Number of weeks";
 
         public const string PocetKocekCS = "Počet koček";
-        private const string PocetKocekEN = "Number of cats";
+        public const string PocetKocekEN = "Number of cats";
 
         public const string MnozstviMasaNaKockuCS = "Množství masa na kočku za týden";
-        private const string MnozstviMasaNaKockuEN = "Meat per cat per week";
+        public const string MnozstviMasaNaKockuEN = "Meat per cat per week";
 
         public const string VysledekZpravaCS = "Kočky za {0} týdnů sní {1} kilogramů masa.";
-        private const string VysledekZpravaEN = "In {0} weeks the adorable little tigers consume {1} kilograms of meat.";
+        public const string VysledekZpravaEN = "In {0} weeks the adorable little tigers consume {1} kilograms of meat.";
 
         public const string VypocitejVysledekCS = "Vypočítej";
+        public const string VypocitejVysledekEN = "Calculate";
 
-        public const string SpatnyFormatCS = "V některém poli není číslo!";
+        public const string SpatnyFormatCS = "V některém poli není celé číslo!";
+        public const string SpatnyFormatEN = "Some field doesn't contain an integer!";
     }
 }
